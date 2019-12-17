cedbd8f0ab46a0fd332eafdc90cc2681825429e6
Nandi Wong <nandi@kintohub.com>
2019-12-17 10:46:02 +0800

    update scripts

--- static-website-build-dockerfile/build.js
+++ static-website-build-dockerfile/build.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 mkdirp('./build')
 
-let content = fs.readFileSync('web/index.html').toString();
+let content = fs.readFileSync('./web/index.html').toString();
 content = content.replace('Anonymous', process.env.USERNAME || 'Nobody')
 
-fs.writeFileSync('build/index.html', content);
+fs.writeFileSync('./build/index.html', content);
